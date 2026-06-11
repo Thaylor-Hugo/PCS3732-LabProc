483d27bbafb9c753b687c7dc863bd4b94fbb18f5
Thaylor-Hugo <thaylorhugo@gmail.com>
2026-06-11 14:39:58 -0300

    🚀 Aula funcionando

--- aula05/aula05.ino
+++ aula05/aula05.ino
@@ -10,8 +10,8 @@ constexpr uint8_t kLedPin = 4;
 constexpr uint8_t kServoPin = 5;
 
 constexpr uint32_t kLedFrequency = 5000;
-constexpr uint8_t kLedResolutionBits = 16;
-constexpr uint32_t kLedMinFrequency = 500;
+constexpr uint8_t kLedResolutionBits = 12;
+constexpr uint32_t kLedMinFrequency = 0;
 constexpr uint32_t kLedMaxFrequency = 10000;
 
 constexpr uint16_t kServoMinUs = 500;
@@ -19,6 +19,7 @@ constexpr uint16_t kServoMaxUs = 2500;
 
 WebServer server(80);
 Servo servoMotor;
+ESP32PWM ledPwm;
 
 int currentLedDuty = 0;
 int currentServoAngle = 0;
@@ -88,10 +89,8 @@ uint32_t readLedFrequency() {
 void applyLedDuty(int dutyCycle, uint32_t frequencyHz) {
 	currentLedDuty = clampPercent(dutyCycle);
 	currentLedFrequency = frequencyHz;
-	ledcAttach(kLedPin, currentLedFrequency, kLedResolutionBits);
-	const uint32_t maxDuty = (1UL << kLedResolutionBits) - 1;
-	const uint32_t pwmValue = map(currentLedDuty, 0, 100, 0, maxDuty);
-	ledcWrite(kLedPin, pwmValue);
+    const float dutyScaled = currentLedDuty / 100.0f;
+    ledPwm.adjustFrequency(currentLedFrequency, dutyScaled);
 }
 
 void applyServoAngle(int angle) {
@@ -169,14 +168,17 @@ void setup() {
 	Serial.begin(115200);
 	delay(200);
 
-	pinMode(kLedPin, OUTPUT);
-	ledcAttach(kLedPin, kLedFrequency, kLedResolutionBits);
+    ESP32PWM allocateTimer(0);
+    ESP32PWM allocateTimer(1);
 
 	servoMotor.setPeriodHertz(50);
 	servoMotor.attach(kServoPin, kServoMinUs, kServoMaxUs);
+	
 
-	applyLedDuty(0, kLedFrequency);
+    ledPwm.attachPin(kLedPin, kLedFrequency, kLedResolutionBits);
+
 	applyServoAngle(0);
+	applyLedDuty(0, kLedFrequency);
 
 	WiFi.mode(WIFI_AP);
 	WiFi.softAP(kApSsid, kApPassword);
